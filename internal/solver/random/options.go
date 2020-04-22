af0e467252e887838f1196b389d364c9b0f39a28
mhv2109 <marshall.vanloon@gmail.com>
2020-04-21 20:38:43 -0400

    remove unused option

--- internal/solver/random/options.go
+++ internal/solver/random/options.go
@@ -5,22 +5,14 @@ import (
 )
 
 func availableOptions() []*solver.Option {
-	options := make([]*solver.Option, 2, 2)
-
-	HashOption := &solver.Option{
-		Name:    "hash",
-		Type:    solver.OptionSpinType,
-		Default: "1024",
-		Min:     "1024",
-		Max:     "4096"}
+	options := make([]*solver.Option, 1, 1)
 
 	UCI_EngineAboutOption := &solver.Option{
 		Name:    "UCI_EngineAboutOption",
 		Type:    solver.OptionStringType,
 		Default: "A UCI Chess engine written in Go by mhv2109"}
 
-	options[0] = HashOption
-	options[1] = UCI_EngineAboutOption
+	options[0] = UCI_EngineAboutOption
 
 	return options
 }
